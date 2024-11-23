c6152e966815f8b3ea71bcdbea4dab1e64f48d95
Elbaton1 <Michael.o'brien@keyin.com>
2024-11-23 19:25:42 -0330

    update

---
+++ runscraper.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the directory containing the Python script
+cd /d "C:\Users\mobri\Documents\School\mun-job-listings"
+
+REM Run the Python script using Python interpreter
+python scraper.py
+
+REM Pause to keep the command window open after execution
+pause
